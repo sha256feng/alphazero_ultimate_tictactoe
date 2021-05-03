9fe6a605b97941c09f356768df0a67388ae6327a
Sha256 <shaalltime@gmail.com>
2021-05-03 16:33:23 -0400

    add NN for ultimate tictactoe

--- src/alpha_net_uttt.py
+++ src/alpha_net_uttt.py
@@ -23,12 +23,12 @@ class board_data(Dataset):
 class ConvBlock(nn.Module):
     def __init__(self):
         super(ConvBlock, self).__init__()
-        self.action_size = 7
+        self.action_size = 81
         self.conv1 = nn.Conv2d(3, 128, 3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(128)
 
     def forward(self, s):
-        s = s.view(-1, 3, 6, 7)  # batch_size x channels x board_x x board_y
+        s = s.view(-1, 3, 9, 9)  # batch_size x channels x board_x x board_y
         s = F.relu(self.bn1(self.conv1(s)))
         return s
 
@@ -57,22 +57,22 @@ class OutBlock(nn.Module):
         super(OutBlock, self).__init__()
         self.conv = nn.Conv2d(128, 3, kernel_size=1) # value head
         self.bn = nn.BatchNorm2d(3)
-        self.fc1 = nn.Linear(3*6*7, 32)
+        self.fc1 = nn.Linear(3*9*9, 32)
         self.fc2 = nn.Linear(32, 1)
         
         self.conv1 = nn.Conv2d(128, 32, kernel_size=1) # policy head
         self.bn1 = nn.BatchNorm2d(32)
         self.logsoftmax = nn.LogSoftmax(dim=1)
-        self.fc = nn.Linear(6*7*32, 7)
+        self.fc = nn.Linear(9*9*32, 81)
     
     def forward(self,s):
         v = F.relu(self.bn(self.conv(s))) # value head
-        v = v.view(-1, 3*6*7)  # batch_size X channel X height X width
+        v = v.view(-1, 3*9*9)  # batch_size X channel X height X width
         v = F.relu(self.fc1(v))
         v = torch.tanh(self.fc2(v))
         
         p = F.relu(self.bn1(self.conv1(s))) # policy head
-        p = p.view(-1, 6*7*32)
+        p = p.view(-1, 9*9*32)
         p = self.fc(p)
         p = self.logsoftmax(p).exp()
         return p, v
